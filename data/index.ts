4bdc19dc823a9c83bc1c2c91b2ae1dfad5a21896
ni-hal <nihalkoyamon93@gmail.com>
2026-03-18 17:41:27 +0530

    Initial commit

---
+++ data/index.ts
@@ -0,0 +1,204 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently Looking New Oppurtunities",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Open to work",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "HRM Software",
+    des: "A comprehensive Human Resource Management (HRM) software designed to streamline HR operations for businesses.",
+    img: "/p3.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg",  "/fm.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "CRM Software",
+    des: "A comprehensive Customer Relationship Management (CRM) software designed to streamline customer interactions and sales processes for businesses.",
+    img: "/p1.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/c.svg"],
+    link: "/",
+  },
+  {
+    id: 3,
+    title: "Saloon Management",
+    des: "A comprehensive Saloon Management software designed to streamline saloon operations for businesses.",
+    img: "/p2.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg",],
+    link: "/",
+  },
+  {
+    id: 4,
+    title: "E-Commerce Website",
+    des: "A comprehensive E-Commerce website designed to streamline e-commerce operations for businesses.",
+    img: "",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg",],
+    link: "/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with NIhal was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. NIhal's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, NIhal is the ideal partner.",
+    name: "lorem",
+    title: "",
+  },
+  {
+    quote:
+      "Collaborating with NIhal was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. NIhal's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, NIhal is the ideal partner.",
+    name: "lorem",
+      title: "lorem",
+    },
+    {
+      quote:
+        "Collaborating with NIhal was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. NIhal's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, NIhal is the ideal partner.",
+    name: "lorem",
+    title: "lorem",
+  },
+  {
+    quote:
+      "Collaborating with NIhal was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. NIhal's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, NIhal is the ideal partner.",
+    name: "lorem",
+    title: "lorem",
+  },
+  {
+    quote:
+      "Collaborating with NIhal was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. NIhal's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, NIhal is the ideal partner.",
+    name: "lorem",
+    title: "lorem",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+
+
+  {
+    id: 2,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Backend Developer ",
+    desc: "Designed and developed scalable backend services using Node.js and Express, implemented REST APIs, handled database operations, and ensured secure and efficient data processing.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance web Developer Project",
+    desc: "Led the dev of a Web Developer for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Full Stack Developer",
+    desc: "Developed and maintained user-facing features using modern Technologies",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/ni-hal",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "https://github.com/ni-hal",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/nihal-puliyakkady",
+  },
+];
